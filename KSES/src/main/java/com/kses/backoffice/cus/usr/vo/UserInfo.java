560229e0ba7f6c464344fc4af3ed7876645e9096
DaeHanJang <eogks196@atensys.co.kr>
2021-12-06 19:06:56 +0900

    merge 테스트

--- KSES/src/main/java/com/kses/backoffice/cus/usr/vo/UserInfo.java
+++ KSES/src/main/java/com/kses/backoffice/cus/usr/vo/UserInfo.java
@@ -12,6 +12,11 @@ public class UserInfo {
 	private String userPhone = "";
 	private String userSexMf = "";
 	private String userBirthDy = "";
+	private String indvdlinfoAgreYn = "";
+	private String indvdlinfoAgreDt = "";
+	private String userRcptYn = "";
+	private String userRcptDvsn = "";
+	private String userRcptNumber = "";
 	private String lastUpdtDtm = "";
 	private String lastUpdusrId = "";
 	private String mode = "";
