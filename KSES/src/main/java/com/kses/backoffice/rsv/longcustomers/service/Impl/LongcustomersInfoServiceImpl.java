d34c3f1da41bc2251ee3cfe456b733b740e6de83
ghdrhkdvy55 <ghdrhkdvy55@naver.com>
2022-01-05 23:11:38 +0900

    장기 예약 현황 페이지 추가

--- KSES/src/main/java/com/kses/backoffice/rsv/longcustomers/service/Impl/LongcustomersInfoServiceImpl.java
+++ KSES/src/main/java/com/kses/backoffice/rsv/longcustomers/service/Impl/LongcustomersInfoServiceImpl.java
@@ -1,12 +1,29 @@
 package com.kses.backoffice.rsv.longcustomers.service.Impl;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kses.backoffice.rsv.longcustomers.mapper.LongcustomersInfoManageMapper;
 import com.kses.backoffice.rsv.longcustomers.service.LongcustomersInfoService;
 
 import egovframework.rte.fdl.cmmn.EgovAbstractServiceImpl;
 
 @Service
 public class LongcustomersInfoServiceImpl extends EgovAbstractServiceImpl implements LongcustomersInfoService{
+	
+	@Autowired
+	private LongcustomersInfoManageMapper longcustomerMapper;
 
+	@Override
+	public List<Map<String, Object>> selectLongcustomerList(Map<String, Object> searchVO) throws Exception {
+		return longcustomerMapper.selectLongcustomerList(searchVO);
+	}
+	
+	@Override
+	public List<?> selectLongcustomerResvList(String longResvSeq) throws Exception {
+        return longcustomerMapper.selectLongcustomerResvList(longResvSeq);
+	}
 }
