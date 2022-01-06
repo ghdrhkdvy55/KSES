d21f8bba3e307409274a6607cdb6dfdd44c37ca1
방 문건 <guntop83@gmail.com>
2022-01-07 00:46:23 +0900

    로그 남기는 방식(포맷) 변경

--- KSES/src/main/java/com/kses/backoffice/sym/log/service/SysLogAspect.java
+++ KSES/src/main/java/com/kses/backoffice/sym/log/service/SysLogAspect.java
@@ -81,7 +81,7 @@ public class SysLogAspect {
 				.filter(not(BeanPropertyBindingResult.class::isInstance))
 				.forEach(arg -> {
 					try {
-						log.info("  (" + joinPoint.getSignature().getName() + ") Controller Parameters: " + objectMapper.writeValueAsString(arg));
+						log.info(" (" + joinPoint.getSignature().getName() + ") Controller Parameters: " + objectMapper.writeValueAsString(arg));
 					} catch (JsonProcessingException e) {
 						log.info(" (" + joinPoint.getSignature().getName() + ") Controller Parameters: " + arg);
 					}
@@ -101,7 +101,7 @@ public class SysLogAspect {
 			if (result instanceof ModelAndView  && result != null) {
 				ModelAndView mav = ((ModelAndView) result);
 				if (!mav.getModel().isEmpty()) {
-					log.info(" ["+ clazz.getSimpleName() +"]\n(" + joinPoint.getSignature().getName() + ") Controller Return: " + mav.getModel());
+					log.info(" (" + joinPoint.getSignature().getName() + ") Controller Return: ["+ clazz.getSimpleName() +"]\n" + mav.getModel());
 				}
 			}
 			
@@ -148,7 +148,7 @@ public class SysLogAspect {
 				.filter(not(BeanPropertyBindingResult.class::isInstance))
 				.forEach(arg -> {
 					try {
-						log.info("  (" + joinPoint.getSignature().getName() + ") Controller Parameters: " + objectMapper.writeValueAsString(arg));
+						log.info(" (" + joinPoint.getSignature().getName() + ") Controller Parameters: " + objectMapper.writeValueAsString(arg));
 					} catch (JsonProcessingException e) {
 						log.info(" (" + joinPoint.getSignature().getName() + ") Controller Parameters: " + arg);
 					}
@@ -168,7 +168,7 @@ public class SysLogAspect {
 			if (result instanceof ModelAndView  && result != null) {
 				ModelAndView mav = ((ModelAndView) result);
 				if (!mav.getModel().isEmpty()) {
-					log.info(" ["+ clazz.getSimpleName() +"]\n(" + joinPoint.getSignature().getName() + ") Controller Return: " + mav.getModel());
+					log.info(" (" + joinPoint.getSignature().getName() + ") Controller Return: ["+ clazz.getSimpleName() +"]\n" + mav.getModel());
 				}
 			}
 			
@@ -220,7 +220,7 @@ public class SysLogAspect {
 			if (result instanceof ModelAndView  && result != null) {
 				ModelAndView mav = ((ModelAndView) result);
 				if (!mav.getModel().isEmpty()) {
-					log.info(" ["+ clazz.getSimpleName() +"]\n(" + joinPoint.getSignature().getName() + ") Controller Return: " + mav.getModel());
+					log.info(" (" + joinPoint.getSignature().getName() + ") Controller Return: ["+ clazz.getSimpleName() +"]\n" + mav.getModel());
 				}
 			}
 			
@@ -251,10 +251,11 @@ public class SysLogAspect {
 		     + " and !@target(com.kses.backoffice.sym.log.annotation.NoLogging)"
 		     + " and !@annotation(com.kses.backoffice.sym.log.annotation.NoLogging))", throwing = "error")
 	public void logUpdateThrow(JoinPoint joinPoint, Exception error) throws Exception  {
+		Class<?> clazz = joinPoint.getTarget().getClass();
 		if (error.getClass().equals(MyBatisSystemException.class) || error.getClass().getName().contains("org.springframework.jdbc")) {
-			log.error(" (" + joinPoint.getSignature().getName() + ") Implement Throwable: " + error.getMessage());
+			log.error(" (" + joinPoint.getSignature().getName() + ") Implement Throwable: ["+ clazz.getSimpleName() +"]\n" + error.getMessage());
 		} else {
-			log.error(" (" + joinPoint.getSignature().getName() + ") Implement Throwable: " + error);
+			log.error(" (" + joinPoint.getSignature().getName() + ") Implement Throwable: ["+ clazz.getSimpleName() +"]\n" + error);
 		}
 	}
 	
