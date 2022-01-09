c3c9cfc4fd0080ff9507c5ce579df110a7827d91
ghdrhkdvy55 <ghdrhkdvy55@naver.com>
2022-01-09 14:12:23 +0900

    KioskInfoService -> selectKioskInfoDetail 주석 처리 insertKioskInfo 추가 KioskInfoServiceImpl -> selectKioskInfoDetail 주석 처리 insertKioskInfo 추가 updateKioskInfo: int ret, if 문 제거, 중복체크 기능 제거(컨트롤러에 기능 중복) KioskInfoManageController -> 필요 없는 매개 변수 제거, try catch문 제거, selectKioskInfoDetail 주석처리, 시리얼 중복 체크 message-common.properties에 추가 해야 할지 검토 부탁드립니다. KioskInfoManagerMapper.xml -> selectKioskInfoList 오더 바이 수정(RNUM),  CENTER_CD, FLOOR_CD, CODE,  USE_YN_VALUE 추가(상세정보에 가져올 데이터)검토 부탁드립니다. selectKioskInfoDetail 주석 처리 kioskList -> fnFloorChange(): 공통으로 쓰일 함수 일지 확인 부탁 드리겠습니다.

--- KSES/src/main/java/com/kses/backoffice/bas/kiosk/mapper/KioskInfoManageMapper.java
+++ KSES/src/main/java/com/kses/backoffice/bas/kiosk/mapper/KioskInfoManageMapper.java
@@ -13,11 +13,11 @@ import egovframework.rte.psl.dataaccess.mapper.Mapper;
 public interface KioskInfoManageMapper {
 	public List<Map<String, Object>> selectKioskInfoList(@Param("params") Map<String, Object> params);
 	
-    public Map<String, Object> selectKioskInfoDetail(String ticketMchnSno);
+//    public Map<String, Object> selectKioskInfoDetail(String ticketMchnSno);
     
-    public int insertKioskInfo(KioskInfo vo);
+    public int insertKioskInfo(KioskInfo kioskInfo);
 	
-    public int updateKioskInfo(KioskInfo vo);
+    public int updateKioskInfo(KioskInfo kioskInfo);
     
     public int deleteKioskInfo(@Param("kioskList") List<String> kioskList);
     
--- KSES/src/main/java/com/kses/backoffice/bas/kiosk/service/KioskInfoService.java
+++ KSES/src/main/java/com/kses/backoffice/bas/kiosk/service/KioskInfoService.java
@@ -7,12 +7,39 @@ import com.kses.backoffice.bas.kiosk.vo.KioskInfo;
 
 public interface KioskInfoService {
 	
+	/**
+	 * KSES 장비 목록 조회
+	 * @param params
+	 * @return
+	 * @throws Exception
+	 */
     List<Map<String, Object>> selectKioskInfoList(Map<String, Object> params) throws Exception;
 	
-    Map<String, Object> selectKioskInfoDetail(String ticketMchnSno) throws Exception;
-	
-    int updateKioskInfo(KioskInfo vo) throws Exception;
 
+//    Map<String, Object> selectKioskInfoDetail(String ticketMchnSno) throws Exception;
+	
+    /**
+     * KSES 장비 정보 수정
+     * @param kioskInfo
+     * @return
+     * @throws Exception
+     */
+    int updateKioskInfo(KioskInfo kioskInfo) throws Exception;
+    
+    /**
+     * KSES 장비 정보 등록
+     * @param kioskInfo
+     * @return
+     * @throws Exception
+     */
+    int insertKioskInfo(KioskInfo kioskInfo) throws Exception;
+
+    /**
+     * KSES 장비 정보 삭제
+     * @param kioskList
+     * @return
+     * @throws Exception
+     */
     int deleteKioskInfo(List<String> kioskList)throws Exception;
     
 }
