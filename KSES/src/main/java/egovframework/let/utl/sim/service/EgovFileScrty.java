11340cd61c38a3c23bfce6621a84f56c3f66df9f
DaeHanJang <eogks196@atensys.co.kr>
2022-01-14 11:28:20 +0900

    시큐어코딩 -> CWE-759(솔트 없이 일방향 해쉬함수 사용) -> 미 사용 함수 제거

--- KSES/src/main/java/egovframework/let/utl/sim/service/EgovFileScrty.java
+++ KSES/src/main/java/egovframework/let/utl/sim/service/EgovFileScrty.java
@@ -198,48 +198,6 @@ public class EgovFileScrty {
 	public static String decode(String data) throws Exception {
 		return new String(decodeBinary(data));
 	}
-
-    /**
-     * 비밀번호를 암호화하는 기능(복호화가 되면 안되므로 SHA-256 인코딩 방식 적용).
-     * 
-     * deprecated : 보안 강화를 위하여 salt로 ID를 지정하는 encryptPassword(password, id) 사용
-     *
-     * @param String data 암호화할 비밀번호
-     * @return String result 암호화된 비밀번호
-     * @exception Exception
-     */
-    @Deprecated
-    public static String encryptPassword(String data) throws Exception {
-
-		if (data == null) {
-		    return "";
-		}
-	
-		byte[] plainText = null; // 평문
-		byte[] hashValue = null; // 해쉬값
-		plainText = data.getBytes();
-	
-		MessageDigest md = MessageDigest.getInstance("SHA-256");
-		
-		// 변경 시 기존 hash 값에 검증 불가.. => deprecated 시키고 유지
-		/*	
-	    // Random 방식의 salt 추가
-	    SecureRandom ng = new SecureRandom();
-	    byte[] randomBytes = new byte[16];
-	    ng.nextBytes(randomBytes);
-	    
-	    md.reset();
-	    md.update(randomBytes);
-	    
-		*/		
-		hashValue = md.digest(plainText);
-		
-		/*
-		BASE64Encoder encoder = new BASE64Encoder();
-		return encoder.encode(hashValue);
-		*/
-		return new String(Base64.encodeBase64(hashValue));
-    }
     
     /**
      * 비밀번호를 암호화하는 기능(복호화가 되면 안되므로 SHA-256 인코딩 방식 적용)
