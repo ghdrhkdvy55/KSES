e4e1c8da4d32b8de7da0427273c7c69452708209
DaeHanJang <eogks196@atensys.co.kr>
2022-03-08 10:43:32 +0900

    불필요한 소스파일 삭제 (UserLogin패키지)

--- KSES/src/main/java/com/kses/front/login/service/UserLoginService.java
+++
@@ -1,9 +0,0 @@
-package com.kses.front.login.service;
-
-import java.util.Map;
-
-public interface UserLoginService {
-	Map<String, Object> getUserLoginInfo(Map<String, Object> params) throws Exception;
-	
-	String getUserLoginCheck(Map<String, Object> params) throws Exception;
-}
