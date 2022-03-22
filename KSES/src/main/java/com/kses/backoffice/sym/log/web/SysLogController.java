885caf47f6abc1bd2352309e27134953121d032c
css0708 <skstjdtns@atensys.co.kr>
2022-03-22 17:32:15 +0900

    시스템 로그 리팩토링 작업 완료

--- KSES/src/main/java/com/kses/backoffice/sym/log/web/SysLogController.java
+++ KSES/src/main/java/com/kses/backoffice/sym/log/web/SysLogController.java
@@ -49,83 +49,57 @@ public class SysLogController {
 	 */
 	@NoLogging
 	@RequestMapping(value="syslogList.do", method = RequestMethod.GET)
-	public ModelAndView viewSyslogList()throws Exception {
+	public ModelAndView viewSyslogList() throws Exception {
 	      return new ModelAndView("/backoffice/sys/syslogList");
 	}
 	
 	@NoLogging
-	@RequestMapping(value="selectlogListAjax.do")
-	public ModelAndView selectlogListAjax(@ModelAttribute("loginVO") LoginVO loginVO
-                                          , @RequestBody Map<String, Object>  searchVO
-                                          , HttpServletRequest request
-										  , BindingResult bindingResult)throws Exception {
-		
-		      ModelAndView model = new ModelAndView(Globals.JSONVIEW);
-		      try{
-		    	  
-		    	 Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
-		 	     if(!isAuthenticated) {
-		 	    	model.addObject(Globals.STATUS_MESSAGE, egovMessageSource.getMessage("fail.common.login"));
-	    			model.addObject(Globals.STATUS, Globals.STATUS_LOGINFAIL);
-	    			return model;
-		 	     }else{
-		 	    	 
-		 	    	 
-		 	    	int pageUnit = searchVO.get("pageUnit") == null ? propertiesService.getInt("pageUnit") : Integer.valueOf((String) searchVO.get("pageUnit"));
-					
-					PaginationInfo paginationInfo = new PaginationInfo();
-					paginationInfo.setCurrentPageNo( Integer.parseInt( SmartUtil.NVL(searchVO.get("pageIndex"), "1")));
-					paginationInfo.setRecordCountPerPage(pageUnit);
-					paginationInfo.setPageSize(propertiesService.getInt("pageSize"));
-					
-					
+	@RequestMapping(value="selectlogListAjax.do", method = RequestMethod.POST)
+	public ModelAndView selectlogListAjax(@RequestBody Map<String, Object> searchVO) throws Exception {
+		ModelAndView model = new ModelAndView(Globals.JSONVIEW);
 
-					searchVO.put("pageSize", propertiesService.getInt("pageSize"));
-					searchVO.put("firstIndex", paginationInfo.getFirstRecordIndex());
-					searchVO.put("lastRecordIndex", paginationInfo.getLastRecordIndex());
-					searchVO.put("recordCountPerPage", paginationInfo.getRecordCountPerPage());
-					searchVO.put("searchBgnDe", SmartUtil.NVL(searchVO.get("searchBgnDe"), "").toString());
-					searchVO.put("searchEndDe", SmartUtil.NVL(searchVO.get("searchEndDe"), "").toString());
-					
-					
+		int pageUnit = searchVO.get("pageUnit") == null ? propertiesService.getInt("pageUnit") : Integer.valueOf((String) searchVO.get("pageUnit"));
+			
+		PaginationInfo paginationInfo = new PaginationInfo();
+		paginationInfo.setCurrentPageNo( Integer.parseInt( SmartUtil.NVL(searchVO.get("pageIndex"), "1")));
+		paginationInfo.setRecordCountPerPage(pageUnit);
+		paginationInfo.setPageSize(propertiesService.getInt("pageSize"));
 
-					List<Map<String, Object>> loginInfo =  sysLogService.selectSysLogList(searchVO);
-					int totCnt = loginInfo.size() > 0 ? Integer.valueOf( loginInfo.get(0).get("total_record_count").toString()) : 0;
-					
-					model.addObject(Globals.JSON_RETURN_RESULTLISR, loginInfo);
-					model.addObject(Globals.PAGE_TOTALCNT, totCnt);
-					paginationInfo.setTotalRecordCount(totCnt);
-					model.addObject(Globals.JSON_PAGEINFO, paginationInfo);
-					model.addObject(Globals.STATUS, Globals.STATUS_SUCCESS);
-					
-					
-		 	     }
-		      }catch(Exception e){
-		    	  StackTraceElement[] ste = e.getStackTrace();
-				  int lineNumber = ste[0].getLineNumber();
-				  log.info("e:" + e.toString() + ":" + lineNumber);
-				  model.addObject(Globals.STATUS, Globals.STATUS_FAIL);
-				  model.addObject(Globals.STATUS_MESSAGE, egovMessageSource.getMessage("fail.common.msg"));
-		      }
-		      return model;
+		searchVO.put("pageSize", propertiesService.getInt("pageSize"));
+		searchVO.put("firstIndex", paginationInfo.getFirstRecordIndex());
+		searchVO.put("lastRecordIndex", paginationInfo.getLastRecordIndex());
+		searchVO.put("recordCountPerPage", paginationInfo.getRecordCountPerPage());
+		searchVO.put("searchBgnDe", SmartUtil.NVL(searchVO.get("searchBgnDe"), "").toString());
+		searchVO.put("searchEndDe", SmartUtil.NVL(searchVO.get("searchEndDe"), "").toString());
+
+		List<Map<String, Object>> loginInfo =  sysLogService.selectSysLogList(searchVO);
+		int totCnt = loginInfo.size() > 0 ? Integer.valueOf( loginInfo.get(0).get("total_record_count").toString()) : 0;
+			
+		model.addObject(Globals.JSON_RETURN_RESULTLISR, loginInfo);
+		model.addObject(Globals.PAGE_TOTALCNT, totCnt);
+		paginationInfo.setTotalRecordCount(totCnt);
+		model.addObject(Globals.JSON_PAGEINFO, paginationInfo);
+		model.addObject(Globals.STATUS, Globals.STATUS_SUCCESS);
+
+		return model;
 	}
-	@NoLogging
-	@RequestMapping(value="selectlogInfoDetail.do")
-	public ModelAndView selectlogInfoDetail(HttpServletRequest request, 
-			                             @RequestParam("requstId") String  requstId)throws Exception {
-		ModelAndView mav = new ModelAndView(Globals.JSONVIEW);
-		try{ 
-			 mav.addObject(Globals.STATUS, Globals.STATUS_SUCCESS);
-			 mav.addObject(Globals.STATUS_REGINFO, sysLogService.selectSysLogInfo(requstId));
-		}catch(Exception e){
-			StackTraceElement[] ste = e.getStackTrace();
-			int lineNumber = ste[0].getLineNumber();
-			log.info("e:" + e.toString() + ":" + lineNumber);
-			mav.addObject(Globals.STATUS_MESSAGE,  egovMessageSource.getMessage("fail.common.select"));
-	    	mav.addObject(Globals.STATUS, Globals.STATUS_FAIL);
-	    	//throw e;
-		}
-		return mav;
-	}
+//	@NoLogging
+//	@RequestMapping(value="selectlogInfoDetail.do")
+//	public ModelAndView selectlogInfoDetail(HttpServletRequest request, 
+//			                             @RequestParam("requstId") String  requstId)throws Exception {
+//		ModelAndView mav = new ModelAndView(Globals.JSONVIEW);
+//		try{ 
+//			 mav.addObject(Globals.STATUS, Globals.STATUS_SUCCESS);
+//			 mav.addObject(Globals.STATUS_REGINFO, sysLogService.selectSysLogInfo(requstId));
+//		}catch(Exception e){
+//			StackTraceElement[] ste = e.getStackTrace();
+//			int lineNumber = ste[0].getLineNumber();
+//			log.info("e:" + e.toString() + ":" + lineNumber);
+//			mav.addObject(Globals.STATUS_MESSAGE,  egovMessageSource.getMessage("fail.common.select"));
+//	    	mav.addObject(Globals.STATUS, Globals.STATUS_FAIL);
+//	    	//throw e;
+//		}
+//		return mav;
+//	}
 	
 }
