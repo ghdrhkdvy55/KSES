acbf0fec02a57b4b63940e759e384493e4f87876
DaeHanJang <eogks196@atensys.co.kr>
2022-03-28 12:04:02 +0900

    예약번호  > 일자별 예약번호 관련 알림톡 예약번호 repalce처리

--- KSES/src/main/java/com/kses/batch/Scheduler.java
+++ KSES/src/main/java/com/kses/batch/Scheduler.java
@@ -87,7 +87,7 @@ public class Scheduler {
 								LOGGER.info("예약번호 : " + resvSeq + " " + i + "차 자동취소 시작" );
 								
 								if(i == 2 && resvPayDvsn.equals("RESV_PAY_DVSN_2") && resvTicketDvsn.equals("RESV_TICKET_DVSN_1")) {
-									ModelMap result = interfaceService.SpeedOnPayMentCancel(resvSeq, "", false);
+									ModelMap result = interfaceService.SpeedOnPayMentCancel(resvSeq, "", false, false);
 	
 									if(!SmartUtil.NVL(result.get(Globals.STATUS), "").equals("SUCCESS")) {
 										LOGGER.info("예약번호 : " + resvSeq + " 결제취소실패");
