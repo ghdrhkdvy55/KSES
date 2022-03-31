112677e3bef5fe8bc01410a9890a3e15b86da55a
DaeHanJang <eogks196@atensys.co.kr>
2022-03-31 12:23:02 +0900

    공통 > 불필요한 import 제거

--- KSES/src/main/java/com/kses/backoffice/bld/center/web/CenterInfoManageController.java
+++ KSES/src/main/java/com/kses/backoffice/bld/center/web/CenterInfoManageController.java
@@ -21,7 +21,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
