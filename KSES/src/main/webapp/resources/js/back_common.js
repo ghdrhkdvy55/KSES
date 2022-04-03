e8999386c358caf28f18c508949a4e4b92da3b59
ghdrhkdvy55 <ghdrhkdvy55@naver.com>
2022-04-03 15:49:13 +0900

    체크박스 전체 선택 셀렉터 id  -> name 속성으로 변경

--- KSES/src/main/webapp/resources/js/back_common.js
+++ KSES/src/main/webapp/resources/js/back_common.js
@@ -482,7 +482,7 @@ function fn_CheckboxAllChange(ck_nm, boolean){
 }
 //체크 박스 체크 에 따른 전체 선택 여부 
 function fn_CheckboxAllChangeInfo(ck_boxNm, ck_ChangeNm){
-   var all_ck =  ($("input[id="+ck_boxNm+"]").is(":checked") == true) ? true : false;
+   var all_ck =  ($("input[name="+ck_boxNm+"]").is(":checked") == true) ? true : false;
    $("input[name="+ck_ChangeNm+"]").prop("checked", all_ck);
 }
 function fn_CheckboxChoice(ck_nm, choiceValue){
